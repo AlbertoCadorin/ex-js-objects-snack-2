9422de0f1d9b5a16d2fbaffc7093c66b8dba9410
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-06-25 13:23:53 +0200

    feat: snack 1

--- snack-1/app.js
+++ snack-1/app.js
@@ -1,1 +1,17 @@
 
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // ?
+console.log(secondBurger.name); // ?
+
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+
+//sempre Double Cheese Burger
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+// solo uno
